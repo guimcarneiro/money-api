62f22ae011e06c8615cfb66c3ca33e1ad0a87dc0
Guilherme Carneiro <guilherme.carneiro@ccc.ufcg.edu.br>
2019-11-26 19:48:33 -0300

    create model Categoria

--- src/main/java/com/money/model/Categoria.java
+++ src/main/java/com/money/model/Categoria.java
@@ -1,5 +1,46 @@
 package com.money.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="categoria")
 public class Categoria {
+	
+	@Id
+	private Long id;
+	
+	private String nome;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
 
 }
