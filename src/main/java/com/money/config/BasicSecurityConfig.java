cdc9fddfaad8eac14ea3fef323c0a09d2a9b4b6f
Guilherme Carneiro <guilherme.carneiro@ccc.ufcg.edu.br>
2019-11-29 21:36:24 -0300

    finaliza api and course

--- src/main/java/com/money/config/BasicSecurityConfig.java
+++ src/main/java/com/money/config/BasicSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -11,14 +13,14 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	//Who is logged
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth.inMemoryAuthentication().withUser("admin").password(encoder.encode("admin")).roles("User");
 	}
 	
 	//Who is not logged
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("categorias").permitAll();
-		super.configure(http);
+		http.authorizeRequests().antMatchers("categorias").permitAll().anyRequest().authenticated().and().httpBasic().and().csrf().disable();
 	}
 	
 }
